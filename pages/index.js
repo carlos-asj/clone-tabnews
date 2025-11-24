7a5827d291e26bef503e4dcba894e4b9ce7f99df
CARLOS ALBERTO SILVA JUNIOR <164648114+carlos-asj@users.noreply.github.com>
2025-11-24 23:48:24 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>Te amo, gatinha! 💘</h1>
+function Home() {
+  return <h1>Te amo, gatinha! 💘</h1>;
+}
+
+function teste() {
+  console.log("indentação errada!");
 }
 
 export default Home;
