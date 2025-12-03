18d68f61c55030d91f6cfd00472b78cf45cec16d
CARLOS ALBERTO SILVA JUNIOR <164648114+carlos-asj@users.noreply.github.com>
2025-12-03 22:33:17 +0000

    add a more robust ssl configuration method

--- testes/integration/api/v1/status/get.test.js
+++ testes/integration/api/v1/status/get.test.js
@@ -7,7 +7,9 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.11");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.version).toContain("16.10");
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(
+    100,
+  );
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
